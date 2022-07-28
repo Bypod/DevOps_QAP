4bb4c94f38a5783deea8ff4bd01bf15858203285
Bypod <94805286+Bypod@users.noreply.github.com>
2022-07-28 00:47:17 -0230

    added new class file

---
+++ job.java
@@ -0,0 +1,11 @@
+public class job {
+    private String name;
+    private String title;
+    private String company;
+
+    public job(String name, String title, String company){
+        this.name = name;
+        this.title = title;
+        this.company = company;
+    }
+}
